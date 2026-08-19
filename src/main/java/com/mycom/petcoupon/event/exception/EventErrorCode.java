1957b65d2789ff790b19d6300547e2a631d567ad
Catverdose <128711486+Catverdose@users.noreply.github.com>
2026-08-19 15:00:03 +0900

    feat: 에러코드 추가

--- src/main/java/com/mycom/petcoupon/event/exception/EventErrorCode.java
+++ src/main/java/com/mycom/petcoupon/event/exception/EventErrorCode.java
@@ -11,6 +11,12 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum EventErrorCode implements BaseErrorCode {
 
+	EVENT_NOT_FOUND(
+			HttpStatus.NOT_FOUND,
+			"EVENT404-0",
+			"이벤트를 찾을 수 없습니다."
+	),
+	
 	INVALID_EVENT_PERIOD(
 			HttpStatus.BAD_REQUEST,
 			"EVENT400-0",
