d9a4c1d118c0b164037abd5eb92d004bd17b5061
shin838 <21stseptemberr@gmail.com>
2026-08-20 13:24:49 +0900

    feat: CouponErrorCode에 쿠폰 사용 관련 에러코드 추가

--- src/main/java/com/mycom/petcoupon/coupon/exception/CouponErrorCode.java
+++ src/main/java/com/mycom/petcoupon/coupon/exception/CouponErrorCode.java
@@ -18,7 +18,10 @@ public enum CouponErrorCode implements BaseErrorCode {
     SOLD_OUT(HttpStatus.CONFLICT, "COUPON409-0", "쿠폰 재고가 모두 소진되었습니다."),
     DUPLICATE_USER(HttpStatus.CONFLICT, "COUPON409-1", "이미 발급받은 쿠폰입니다."),
     DUPLICATE_REQUEST(HttpStatus.CONFLICT, "COUPON409-2", "이미 처리된 요청입니다."),
-    COUPON_NOT_FOUND(HttpStatus.NOT_FOUND, "COUPON404-0", "존재하지 않는 쿠폰입니다.");
+    COUPON_NOT_FOUND(HttpStatus.NOT_FOUND, "COUPON404-0", "존재하지 않는 쿠폰입니다."),
+	COUPON_ISSUE_NOT_FOUND(HttpStatus.NOT_FOUND, "COUPON404-1", "발급 내역을 찾을 수 없습니다."),
+    NOT_COUPON_OWNER(HttpStatus.FORBIDDEN, "COUPON403-0", "본인의 쿠폰이 아닙니다."),
+    INVALID_ISSUE_STATUS(HttpStatus.CONFLICT, "COUPON409-3", "현재 상태에서는 처리할 수 없습니다.");
 
     private final HttpStatus status;
     private final String code;
