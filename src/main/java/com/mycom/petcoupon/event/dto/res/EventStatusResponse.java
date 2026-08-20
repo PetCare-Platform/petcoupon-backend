213923e605f5cc8043e30d054800669995767146
Catverdose <eongpup@gmail.com>
2026-08-20 10:49:30 +0900

    fix: EventStatusResponse builder추가

--- src/main/java/com/mycom/petcoupon/event/dto/res/EventStatusResponse.java
+++ src/main/java/com/mycom/petcoupon/event/dto/res/EventStatusResponse.java
@@ -2,6 +2,9 @@ package com.mycom.petcoupon.event.dto.res;
 
 import com.mycom.petcoupon.event.entity.enums.EventStatus;
 
+import lombok.Builder;
+
+@Builder
 public record EventStatusResponse(
 		Long eventId,
 		EventStatus status
