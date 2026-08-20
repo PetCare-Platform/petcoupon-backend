30cc9a54036cfb80bb4d27f1c82220b54911680b
박수빈 <psb0304022@gmail.com>
2026-08-20 15:31:47 +0900

    [FEAT] 선착순 쿠폰 신청 요청 Redis Stream 적재 로직 구현 (#12) (#25)
    * feat: 쿠폰 신청 Redis Stream Producer 구현
    
    * refactor: 쿠폰 신청 Producer 검증 주석 수정

--- src/main/java/com/mycom/petcoupon/coupon/exception/CouponErrorCode.java
+++ src/main/java/com/mycom/petcoupon/coupon/exception/CouponErrorCode.java
@@ -15,10 +15,16 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum CouponErrorCode implements BaseErrorCode {
+	
+	INVALID_ISSUE_REQUEST(HttpStatus.BAD_REQUEST, "COUPON400-0", "쿠폰 신청 요청값이 올바르지 않습니다."),
+	
     SOLD_OUT(HttpStatus.CONFLICT, "COUPON409-0", "쿠폰 재고가 모두 소진되었습니다."),
     DUPLICATE_USER(HttpStatus.CONFLICT, "COUPON409-1", "이미 발급받은 쿠폰입니다."),
     DUPLICATE_REQUEST(HttpStatus.CONFLICT, "COUPON409-2", "이미 처리된 요청입니다."),
-    COUPON_NOT_FOUND(HttpStatus.NOT_FOUND, "COUPON404-0", "존재하지 않는 쿠폰입니다.");
+    
+    COUPON_NOT_FOUND(HttpStatus.NOT_FOUND, "COUPON404-0", "존재하지 않는 쿠폰입니다."),
+    
+    ISSUE_REQUEST_SAVE_FAILED(HttpStatus.SERVICE_UNAVAILABLE, "COUPON503-0", "쿠폰 신청 요청을 처리하지 못했습니다.");
 
     private final HttpStatus status;
     private final String code;
