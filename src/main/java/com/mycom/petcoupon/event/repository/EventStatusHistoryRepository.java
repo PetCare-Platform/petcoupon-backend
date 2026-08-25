951764020be536730fd277bad3b3b5496edd4159
Catverdose <128711486+Catverdose@users.noreply.github.com>
2026-08-25 11:49:04 +0900

    [FEAT] 이벤트 상태 전환 내부 스케줄러 구현 (#61) (#68)
    * feat: 이벤트 상태 전환 내부 스케줄러 구현
    
    * chore: properties에 스케줄러 설정 추가 스케줄러기본값1분
    
    * fix: 스케쥴러 전용 taskscheduler분리 원자적 조건부 업데이트 사용
    
    * fix: EventStatusHistory 저장 변경,스케줄러테스트 폴링으로 변경
    
    * fix: SchedulingConfigurer 관련 버그 수정
    
    * fix: try/catch 수정

--- src/main/java/com/mycom/petcoupon/event/repository/EventStatusHistoryRepository.java
+++ src/main/java/com/mycom/petcoupon/event/repository/EventStatusHistoryRepository.java
@@ -1,8 +1,12 @@
 package com.mycom.petcoupon.event.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.mycom.petcoupon.event.entity.EventStatusHistory;
 
 public interface EventStatusHistoryRepository extends JpaRepository<EventStatusHistory, Long> {
+
+	List<EventStatusHistory> findByEvent_EventId(Long eventId);
 }
