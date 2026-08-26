d8c5e74e11c3b1c678a64ee168c153e9889af6f4
shin838 <21stseptemberr@gmail.com>
2026-08-26 13:55:56 +0900

    chore: 발생 불가능한 VerificationErrorType.DUPLICATE_CONSUME 삭제

--- src/main/java/com/mycom/petcoupon/reconciliation/entity/enums/VerificationErrorType.java
+++ src/main/java/com/mycom/petcoupon/reconciliation/entity/enums/VerificationErrorType.java
@@ -1,11 +1,10 @@
 package com.mycom.petcoupon.reconciliation.entity.enums;
 
 public enum VerificationErrorType {
-	STOCK_MISMATCH, 
-	DUPLICATE_ISSUE, 
-	INVALID_STATUS, 
-	HISTORY_MISMATCH, 
-	DUPLICATE_CONSUME,
+	STOCK_MISMATCH,
+	DUPLICATE_ISSUE,
+	INVALID_STATUS,
+	HISTORY_MISMATCH,
 	SEQUENCE_GAP,
 	STOCK_NOT_RESTORED
 }
